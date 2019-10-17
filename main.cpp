d1b66b483b0f8a1320ccbc874574a9a74c5a86b2
valencast2000 <47958376+valencast2000@users.noreply.github.com>
2019-10-17 20:08:19 -0300

    Probado Button en main
    Se agrego un par de lineas al main solo para probar el funcionamiento de la funcion isPressed(), perteneciente a la clase Button.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
+#include "includes/Button.h"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "SFML works!");
     // Vector de vectores
     // niv es el nivel
     std::vector<std::vector<int>> niv = {{1, 1, 1, 1, 1, 1}, {1, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 0, 1}, {1, 1, 1, 1, 0, 1}, {1, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 0, 1}, {1, 1, 1, 1, 1, 1}};
@@ -17,14 +18,20 @@ int main() {
     tex.loadFromFile("resources/Imagenes/Mapa.png");
     Mapa map(niv, rot, &tex);
 
+	//Button button(100,200,150,50,"No implementado",sf::Color::Red);
+	
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+			if (event.type == sf::Event::MouseButtonPressed)
+				//if (button.isPressed(&window))
+					//std::cout<<"Boton presionado"<<std::endl;
         }
         window.clear(sf::Color::Green);
         map.dibujar(&window);
+		//button.render(&window);
         window.display();
     }
 
