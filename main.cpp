4fb8e574e11805ead8650d31193574c35ad8a2f0
Your Name <you@example.com>
2019-10-25 17:11:34 -0300

    Agregado de menu

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@
 #include <vector>
 
 int main() {
+	bool jugar=false;
+	
+	sf::Font font;
+	font.loadFromFile("resources/Fuentes/OpenSans-Light.ttf");
+	
     sf::RenderWindow window(sf::VideoMode(1024, 768), "SFML works!");
     // Vector de vectores
     // niv es el tipo de tiles ordenado en columnas
@@ -15,36 +20,68 @@ int main() {
     // rot es la rotacion de cada tile
     std::vector<std::vector<int>> rot = {{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}};
 
+	Button buttonIniciar(100, 325, 150, 75, "Iniciar Juego", sf::Color::Red, font);
+	Button buttonSalir(100, 450, 150, 75, "Salir", sf::Color::Red, font);
+	
+	sf::Text textoIniciar;
+	textoIniciar.setString("Iniciar");
+	textoIniciar.setFont(font);
+	textoIniciar.setCharacterSize(40);
+	textoIniciar.setOrigin(sf::Vector2f((textoIniciar.getGlobalBounds().width)/2,(textoIniciar.getGlobalBounds().height)/2));
+	textoIniciar.setPosition(sf::Vector2f(buttonIniciar.getCenterX(),buttonIniciar.getCenterY()));
+	textoIniciar.setFillColor(sf::Color::Black);
+	
+	sf::Text textoSalir;
+	textoSalir.setString("Salir");
+	textoSalir.setFont(font);
+	textoSalir.setCharacterSize(40);
+	textoSalir.setOrigin(sf::Vector2f((textoSalir.getGlobalBounds().width)/2,(textoSalir.getGlobalBounds().height)/2));
+	textoSalir.setPosition(sf::Vector2f(buttonSalir.getCenterX(),buttonSalir.getCenterY()));
+	textoSalir.setFillColor(sf::Color::Black);
+	
     sf::Texture tex, tChef;
-    tChef.loadFromFile("Imagenes/Chef.png");
-    tex.loadFromFile("Imagenes/Mapa.png");
+    tChef.loadFromFile("resources/Imagenes/Chef.png");
+    tex.loadFromFile("resources/Imagenes/Mapa.png");
+	
     //Se crea el mapa y se mandan tipo de tiles y su rotacion, con  la textura del mapa
     Mapa map(niv, rot, &tex);
     Chef chef(&tChef, 640 / 2 + 300, 480 / 2);
-
-    // Button button(100, 200, 150, 50, "No implementado", sf::Color::Red);
-    window.setFramerateLimit(60);
+    
+	window.setFramerateLimit(60);
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
-            // if (event.type == sf::Event::MouseButtonPressed)
-            //     if (button.isPressed(&window))
-            //         std::cout << "Boton presionado" << std::endl;
-        }
-        window.clear(sf::Color::Green);
-        map.dibujar(&window);
-        // button.render(&window);
+			
+			if (!jugar){
+				if (event.type == sf::Event::MouseButtonPressed) {
+					if (buttonIniciar.isPressed(&window)) {
+						jugar=true;
+					}
+					if (buttonSalir.isPressed(&window))
+						window.close();
+				}
+			}
+		}
+		window.clear(sf::Color::Green);
+		buttonIniciar.render(&window);
+		buttonSalir.render(&window);
+		window.draw(textoIniciar);
+		window.draw(textoSalir);
+		
         bool izq, der, arriba, abajo;
-        izq = Keyboard::isKeyPressed(Keyboard::Left);
-        der = Keyboard::isKeyPressed(Keyboard::Right);
-        arriba = Keyboard::isKeyPressed(Keyboard::Up);
-        abajo = Keyboard::isKeyPressed(Keyboard::Down);
-        chef.mover(izq, der, arriba, abajo);
-
-        chef.dibujar(&window, &map);
-        window.display();
+		if(jugar){
+			izq = Keyboard::isKeyPressed(Keyboard::Left);
+			der = Keyboard::isKeyPressed(Keyboard::Right);
+			arriba = Keyboard::isKeyPressed(Keyboard::Up);
+			abajo = Keyboard::isKeyPressed(Keyboard::Down);
+			chef.mover(izq, der, arriba, abajo);
+			map.dibujar(&window);
+			chef.dibujar(&window, &map);
+		}
+		window.display();
+       
     }
 
     return 0;
--- src/Button.cpp
+++ src/Button.cpp
@@ -5,14 +5,28 @@
 #include <string>
 
 Button::Button(float Pos_x, float Pos_y, float Ancho, float Largo, std::string Text, 
-			   sf::Color buttonColor){
+			   sf::Color buttonColor, sf::Font font){
 	this->shape.setPosition(sf::Vector2f(Pos_x, Pos_y));
 	this->shape.setSize(sf::Vector2f(Ancho, Largo));
 	this->shape.setFillColor(buttonColor);
+	this->text.setString(Text);
+	this->text.setPosition(sf::Vector2f(Pos_x, Pos_y));
+	this->text.setCharacterSize(14);
+	this->text.setFont(font);
+	this->text.setFillColor(sf::Color::White);
+	
 }
 
 Button::~Button(){
 	
+}
+
+float Button::getCenterX() {
+	return (this->shape.getPosition().x + ((this->shape.getSize().x)/2));
+}
+
+float Button::getCenterY() {
+	return (this->shape.getPosition().y + ((this->shape.getSize().y)/2));
 }
 
 bool Button::isPressed(sf::RenderWindow *w) {
@@ -28,4 +42,5 @@ bool Button::isPressed(sf::RenderWindow *w) {
 
 void Button::render(sf::RenderWindow *w) {
 	w->draw(this->shape);
+	w->draw(this->text);
 }
